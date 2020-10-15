84358cc83c9ed0f6e505cd274836f65bb2ee0f97
Folkert van der Beek <folkertvanderbeek@xs4all.nl>
2020-10-15 20:54:51 +0200

    Add headers

--- zotero-auth.el
+++ zotero-auth.el
@@ -1,5 +1,27 @@
 ;;; zotero-auth.el --- Authorization for the Zotero API  -*- lexical-binding: t; -*-
 
+;; Author: Folkert van der Beek <folkertvanderbeek@gmail.com>
+
+;; This file is NOT part of GNU Emacs.
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License along with
+;; this program. If not, see <http://www.gnu.org/licenses/>.
+
+;;; Commentary:
+;; Authorization functions for Zotero. The Oauth library is used, and advised
+;; because the Zotero API needs an access token, secret, user ID and username,
+;; but the original function only fetches the access token and secret.
+
 ;;; Code:
 
 ;;;; Requirements
