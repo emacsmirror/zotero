abd8051f2272df7279b947dd212a9fbfca943157
Folkert van der Beek <folkertvanderbeek@xs4all.nl>
2020-10-15 20:38:34 +0200

    Rearrange functions

--- zotero-auth.el
+++ zotero-auth.el
@@ -11,60 +11,25 @@
 ;; The Client Key and Client Secret for use during all future OAuth handshakes between emacs-zotero and zotero.org.
 (defconst zotero-auth-client-key "28b59774b8e3e022a296"
   "The key issued by Zotero.")
+
 (defconst zotero-auth-client-secret "ed094e305ae7305ebbbc"
   "The secret issued by Zotero.")
 
 ;; The OAuth endpoints for access to the Zotero API:
 (defconst zotero-auth-request-token-endpoint "https://www.zotero.org/oauth/request"
   "Temporary Credential Request.")
+
 (defconst zotero-auth-token-endpoint "https://www.zotero.org/oauth/access"
   "Token Request URI.")
+
 (defconst zotero-auth-authorize-endpoint "https://www.zotero.org/oauth/authorize"
   "Resource Owner Authorization URI.")
 
 (cl-defstruct (zotero-auth-token (:include oauth-t)
-                                        (:constructor zotero-auth-token-create)
-                                        (:copier nil))
+                                 (:constructor zotero-auth-token-create)
+                                 (:copier nil))
   "Specialized token of `oauth-t' including the extra slots `userid' and `username'."
   userid username)
-
-;;;; Functions
-
-(defun zotero-auth--fetch-token (arg)
-  "Advice replacing `oauth-fetch-token' in package `oauth.el'.
-Fetch an access token, secret, user ID and username from the service provider. The original function only fetches the access token and secret."
-  (with-current-buffer (oauth-do-request arg)
-    ;; Move beyond blank line at end of headers.
-    (goto-char (point-min))
-    (while (progn
-             (forward-line 1)
-             (not (looking-at "^\r?\n"))))
-    (forward-line 1)
-    (let ((token (zotero-auth-token-create))
-          (pairs (url-parse-query-string (buffer-substring (point) (point-max)))))
-      (dolist (pair pairs)
-        (cond
-         ((equal (car pair) "oauth_token")
-          (setf (zotero-auth-token-token token) (cadr pair)))
-         ((equal (car pair) "oauth_token_secret")
-          (setf (zotero-auth-token-token-secret token) (cadr pair)))
-         ((equal (car pair) "userID")
-          (setf (zotero-auth-token-userid token) (cadr pair)))
-         ((equal (car pair) "username")
-          (setf (zotero-auth-token-username token) (cadr pair)))))
-      token)))
-
-(defun zotero-auth--before-authorize-function ()
-  "Function to be run before an OAuth authorization request."
-  (advice-add #'oauth-fetch-token :override #'zotero-auth--fetch-token))
-
-(defun zotero-auth--after-authorize-function ()
-  "Function to be run after an OAuth authorization request."
-  (advice-remove #'oauth-fetch-token #'zotero-auth--fetch-token))
-
-(defun zotero-auth--save-token (token)
-  "Save the acces token for future sessions and return it."
-  (customize-save-variable 'zotero-auth-token token))
 
 ;;;; Methods
 
@@ -121,6 +86,44 @@ Zotero will send the userID associated with the key along too."
   "Return the Zotero username.
 Zotero will send the username associated with the key along too."
   (zotero-auth-token-username token))
+
+;;;; Functions
+
+(defun zotero-auth--fetch-token (arg)
+  "Advice replacing `oauth-fetch-token' in package `oauth.el'.
+Fetch an access token, secret, user ID and username from the service provider. The original function only fetches the access token and secret."
+  (with-current-buffer (oauth-do-request arg)
+    ;; Move beyond blank line at end of headers.
+    (goto-char (point-min))
+    (while (progn
+             (forward-line 1)
+             (not (looking-at "^\r?\n"))))
+    (forward-line 1)
+    (let ((token (zotero-auth-token-create))
+          (pairs (url-parse-query-string (buffer-substring (point) (point-max)))))
+      (dolist (pair pairs)
+        (cond
+         ((equal (car pair) "oauth_token")
+          (setf (zotero-auth-token-token token) (cadr pair)))
+         ((equal (car pair) "oauth_token_secret")
+          (setf (zotero-auth-token-token-secret token) (cadr pair)))
+         ((equal (car pair) "userID")
+          (setf (zotero-auth-token-userid token) (cadr pair)))
+         ((equal (car pair) "username")
+          (setf (zotero-auth-token-username token) (cadr pair)))))
+      token)))
+
+(defun zotero-auth--before-authorize-function ()
+  "Function to be run before an OAuth authorization request."
+  (advice-add #'oauth-fetch-token :override #'zotero-auth--fetch-token))
+
+(defun zotero-auth--after-authorize-function ()
+  "Function to be run after an OAuth authorization request."
+  (advice-remove #'oauth-fetch-token #'zotero-auth--fetch-token))
+
+(defun zotero-auth--save-token (token)
+  "Save the acces token for future sessions and return it."
+  (customize-save-variable 'zotero-auth-token token))
 
 (provide 'zotero-auth)
 
