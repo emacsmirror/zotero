1e9a6ac3cc8ead2a6f26474d8d7d1bc1ba69a6e7
Folkert van der Beek <folkertvanderbeek@xs4all.nl>
2021-02-15 15:29:33 +0100

    Put subcollections under their parents

--- zotero-browser.el
+++ zotero-browser.el
@@ -1602,6 +1602,14 @@ With a `C-u' prefix, create a new top level attachment."
                   zotero-browser-id id
                   zotero-browser-keys keys)
             (seq-do (lambda (key) (ewoc-enter-last ewoc key)) keys)
+            (dolist (key keys)
+              ;; Create a new node if key is not a child
+              (unless (zotero-cache-parentcollection key table)
+                (ewoc-enter-last ewoc key))
+              ;; Then create nodes for the children of key
+              (when-let ((children (ht-keys (zotero-cache-subcollections key table))))
+                (dolist (child children)
+                  (ewoc-enter-last ewoc child))))
             (zotero-browser-expand-level zotero-browser-default-collection-level)
             (zotero-browser-items "items-top" nil type id)))))
     buffer))
